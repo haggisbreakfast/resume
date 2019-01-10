56627487435dd17e32e1be1c5ac509abd7d28ced
Haggis Breakfast <lj.stewart00@gmail.com>
2019-01-10 14:48:02 -0800

    makes date formatting uniform

--- src/resume.ts
+++ src/resume.ts
@@ -66,7 +66,7 @@ export const resume: Resume = {
       company: 'Omega Nutrition',
       position: 'Customer Service Representative/Project Coordinator',
       startDate: 'May 2017',
-      endDate: 'Sept. 2018',
+      endDate: 'Sept 2018',
       summary:
         'This role involved providing customer support via chat, email, and phone, managing social media, processing customer orders, utilizing and maintaining Shopify for order processing, data entry and sales analysis.',
     },
@@ -81,8 +81,8 @@ export const resume: Resume = {
     {
       company: 'Monarch House',
       position: 'Behaviour Interventionist',
-      startDate: 'Feb. 2015',
-      endDate: 'Sept. 2015',
+      startDate: 'Feb 2015',
+      endDate: 'Sept 2015',
       summary:
         'As a Behaviour Interventionist I was responsible for administering ABA therapy to children with Autism, working alongside Clinicians, Speech Pathologists and Occupational Therapists to design learning programs for clients, maintaining fluent and consistent communication with parents and Clinicians, and composing client progress reports.',
     },
@@ -90,8 +90,8 @@ export const resume: Resume = {
   volunteer: {
     organization: 'CiTR Radio',
     position: 'Station Volunteer',
-    startDate: 'Sept. 2012',
-    endDate: 'Feb. 2016',
+    startDate: 'Sept 2012',
+    endDate: 'Feb 2016',
     summary:
       'My role involved assisting with fundraising events, digitizing and organizing music libraries, tabling live shows and events, and composing written contributions to station magazine, Discorder.',
   },
@@ -101,42 +101,42 @@ export const resume: Resume = {
       institution: 'Lighthouse Labs',
       program: 'Web Development',
       studyType: 'Diploma',
-      startDate: 'Sept. 2018',
-      endDate: 'Nov. 2018',
+      startDate: 'Sept 2018',
+      endDate: 'Nov 2018',
     },
     {
       institution: 'University of British Columbia',
       program: 'Psychology',
       studyType: 'B.A.',
-      startDate: 'Sept. 2012',
+      startDate: 'Sept 2012',
       endDate: 'May 2014',
     },
   ],
   projects: [
     {
       name: 'Good Synth (collaborative)',
-      date: 'Nov. 2018',
+      date: 'Nov 2018',
       link: 'https://multi-player-piano.herokuapp.com/',
       summary:
         'Good Synth is an interactive, collaborative Synthesizer Interface, which allows you to make music with all other connected users in real-time! It was built with ReactJS, Web Audio API, Express, WebSockets, JSX, ES6, and SCSS. ',
     },
     {
       name: 'Chatty App',
-      date: 'Nov. 2018',
+      date: 'Nov 2018',
       link: 'https://github.com/haggisbreakfast/chatty',
       summary:
         'Chatty App is a simple, single page web app that allows users to chat in real-time. Built with ReactJS, Webpack with Babel, JSX, ES6, WebSockets, and SCSS.',
     },
     {
       name: 'Choo-Chooser (collaborative)',
-      date: 'Oct. 2018',
+      date: 'Oct 2018',
       link: 'https://github.com/haggisbreakfast/Decision-Maker',
       summary:
         'Choo-Chooser is a simple polling web application to aid its users in making (what can be) difficult decisions. Full-stack web application built with HTML, Sass, JS, jQuery, AJAX, EJS, Node, Express, Knex, Mailgun and PostgresSQL',
     },
     {
       name: 'Tweeter',
-      date: 'Sept. 2018',
+      date: 'Sept 2018',
       link: 'https://github.com/haggisbreakfast/tweeter',
       summary:
         'Tweeter is a simple, single-page Twitter clone, built upon the provided starter code for this project to put my HTML, CSS, JS, jQuery and AJAX front-end skills, as well as my Node, Express and MongoDB back-end skills to work.',
